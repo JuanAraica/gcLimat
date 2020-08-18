f602ced74b5b0ae05ab4c2d2205525c49aba14b4
JUAN ARAICA NAVARRO <juan.araica.navarro@est.una.ac.cr>
2020-08-18 13:24:56 -0500

    Primer modulo

---
+++ gcLimat/Controllers/equipoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using gcLimat.Models;
+using gcLimat.Models.ViewModels;
+
+namespace gcLimat.Controllers
+ 
+{
+    public class equipoController : Controller
+    {
+        // GET: equipo
+        public ActionResult Index()
+        {
+            using (gdlimatEntities db= new gdlimatEntities()) {
+                var lst = (from d in db.tbl_EQUIPO
+                           select new ListEquipoViewModel
+                           {
+                               idEquipo=d.idEquipo,
+                               tipoEquipo = d.tipoEquipo,
+                               region = d.region,
+                               potenciaMaxima = d.potenciaMaxima,
+                               serie = d.serie,
+                               marca = d.marca,
+                               subEstacion = d.subEstacion,
+                               tipoRefrigeracion = d.tipoRefrigeracion,
+                               tension = d.tension,
+                               cantidadFase = d.cantidadFase,
+                               cantidadAceite = d.cantidadAceite,
+                               annio = d.annio,
+                               MVA = d.MVA,
+                               relacioTension = d.relacioTension,
+                               humedadRelativa = d.humedadRelativa,
+                               temperaturaAmbiente = d.temperaturaAmbiente,
+                               temperaturaEquipo = d.temperaturaEquipo,
+                               ultimoReporte = d.ultimoReporte,
+                               ultimoMantenimiento = d.ultimoMantenimiento,
+                               ultimoMuestreo = d.ultimoMuestreo
+                           }).ToList();
+            }
+                return View();
+        }
+    }
+}
---
+++ gcLimat/Models/ViewModels/ListEquipoViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gcLimat.Models.ViewModels
+{
+    public class ListEquipoViewModel
+    {
+        public String idEquipo{get;set;}
+        public String tipoEquipo { get; set; }
+        public String region { get; set; }
+        public String potenciaMaxima { get; set; }
+        public String serie { get; set; }
+        public String marca { get; set; }
+        public String subEstacion { get; set; }
+        public String tipoRefrigeracion { get; set; }
+        public String tension { get; set; }
+        public String cantidadFase { get; set; }
+        public String cantidadAceite { get; set; }
+        public String annio { get; set; }
+        public String MVA { get; set; }
+        public String relacioTension { get; set; }
+        public String humedadRelativa { get; set; }
+        public String temperaturaAmbiente { get; set; }
+        public String temperaturaEquipo { get; set; }
+        public String ultimoReporte { get; set; }
+        public String ultimoMantenimiento { get; set; }
+        public String ultimoMuestreo { get; set; }
+    }
+}
