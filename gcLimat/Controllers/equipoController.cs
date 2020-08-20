72ef02f20496c0588e3f8c0d4ee9a52e345d30af
JUAN ARAICA NAVARRO <juan.araica.navarro@est.una.ac.cr>
2020-08-19 18:49:30 -0600

    Modulo equipos completo(no funcional)

--- gcLimat/Controllers/equipoController.cs
+++ gcLimat/Controllers/equipoController.cs
@@ -1,24 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using gcLimat.Models;
 using gcLimat.Models.ViewModels;
 
 namespace gcLimat.Controllers
- 
 {
     public class equipoController : Controller
     {
         // GET: equipo
         public ActionResult Index()
         {
-
             List<ListEquipoViewModel> lst;
-            using (gdlimatEntities db= new gdlimatEntities()) {
+            using (gdlimatEntities db= new gdlimatEntities()) 
+            {
                   lst = (from d in db.tbl_EQUIPO
                            select new ListEquipoViewModel
                            {
@@ -44,19 +41,24 @@ namespace gcLimat.Controllers
                                ultimoMuestreo = d.ultimoMuestreo
                            }).ToList();
             }
-                return View(lst);
+            return View(lst);
         }
-        public ActionResult NuevoEquipo() {
-            return View( );
 
+        public ActionResult NuevoEquipo() 
+        {
+            return View();
         }
+
+        [HttpPost]
         public ActionResult NuevoEquipo(EquipoViewModel model)
         {
-            try {
+            try 
+            {
                 if (ModelState.IsValid) 
                 {
-                    using (gdlimatEntities db = new gdlimatEntities()) { 
-                        var oEquipo = new tbl_EQUIPO();
+                    using (gdlimatEntities db = new gdlimatEntities())
+                    { 
+                    var oEquipo = new tbl_EQUIPO();
                     oEquipo.idEquipo = model.idEquipo;
                     oEquipo.tipoEquipo = model.tipoEquipo;
                     oEquipo.region = model.region;
@@ -80,13 +82,101 @@ namespace gcLimat.Controllers
                         db.tbl_EQUIPO.Add(oEquipo);
                         db.SaveChanges();
                     }
+                    return Redirect("~/equipo/");
                 }
-                return Redirect("~/equipo/");
-            } catch (Exception ex) {
+                return View(model);
+            }
+            catch (Exception ex) 
+            {
                 throw new Exception(ex.Message);
-                    }
-
+            }
         }
 
+        public ActionResult EditarEquipo(String idEquipo)
+        {
+            EquipoViewModel model = new EquipoViewModel();
+            using (gdlimatEntities db = new gdlimatEntities())
+            {
+                var oEquipo = db.tbl_EQUIPO.Find(idEquipo);
+                model.idEquipo = oEquipo.idEquipo;
+                model.tipoEquipo = oEquipo.tipoEquipo;
+                model.region = oEquipo.region;
+                model.potenciaMaxima = oEquipo.potenciaMaxima;
+                model.serie = oEquipo.serie;
+                model.marca = oEquipo.marca;
+                model.subEstacion = oEquipo.subEstacion;
+                model.tipoRefrigeracion = oEquipo.tipoRefrigeracion;
+                model.tension = oEquipo.tension;
+                model.cantidadFase = oEquipo.cantidadFase;
+                model.cantidadAceite = oEquipo.cantidadAceite;
+                model.annio = oEquipo.annio;
+                model.MVA = oEquipo.MVA;
+                model.relacioTension = oEquipo.relacioTension;
+                model.humedadRelativa = oEquipo.humedadRelativa;
+                model.temperaturaAmbiente = oEquipo.temperaturaAmbiente;
+                model.temperaturaEquipo = oEquipo.temperaturaEquipo;
+                model.ultimoReporte = oEquipo.ultimoReporte;
+                model.ultimoMantenimiento = oEquipo.ultimoMantenimiento;
+                model.ultimoMuestreo = oEquipo.ultimoMuestreo;
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditarEquipo(EquipoViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (gdlimatEntities db = new gdlimatEntities())
+                    {
+                        var oEquipo = db.tbl_EQUIPO.Find(model.idEquipo);
+                        oEquipo.idEquipo = model.idEquipo;
+                        oEquipo.tipoEquipo = model.tipoEquipo;
+                        oEquipo.region = model.region;
+                        oEquipo.potenciaMaxima = model.potenciaMaxima;
+                        oEquipo.serie = model.serie;
+                        oEquipo.marca = model.marca;
+                        oEquipo.subEstacion = model.subEstacion;
+                        oEquipo.tipoRefrigeracion = model.tipoRefrigeracion;
+                        oEquipo.tension = model.tension;
+                        oEquipo.cantidadFase = model.cantidadFase;
+                        oEquipo.cantidadAceite = model.cantidadAceite;
+                        oEquipo.annio = model.annio;
+                        oEquipo.MVA = model.MVA;
+                        oEquipo.relacioTension = model.relacioTension;
+                        oEquipo.humedadRelativa = model.humedadRelativa;
+                        oEquipo.temperaturaAmbiente = model.temperaturaAmbiente;
+                        oEquipo.temperaturaEquipo = model.temperaturaEquipo;
+                        oEquipo.ultimoReporte = model.ultimoReporte;
+                        oEquipo.ultimoMantenimiento = model.ultimoMantenimiento;
+                        oEquipo.ultimoMuestreo = model.ultimoMuestreo;
+
+                        db.Entry(oEquipo).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
+
+                    return Redirect("~/equipo/");
+                }
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult EliminarEquipo(String idEquipo)
+        {
+            using (gdlimatEntities db = new gdlimatEntities())
+            {
+                var oEquipo = db.tbl_EQUIPO.Find(idEquipo);
+                db.tbl_EQUIPO.Remove(oEquipo);
+                db.SaveChanges();
+            }
+            return Redirect("~/equipo/");
+        }
     }
 }
